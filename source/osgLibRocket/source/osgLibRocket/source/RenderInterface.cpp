d660f5b6ba38deae5cdbc7b548d1e625d21d67b5
martinscheffler@gmail.com <martinscheffler@gmail.com@fe1f96f2-e110-b4a2-0c9b-42d904da81c5>
2012-02-23 09:23:23 +0000

    a fix got lost in the last commit

--- source/osgLibRocket/source/osgLibRocket/source/RenderInterface.cpp
+++ source/osgLibRocket/source/osgLibRocket/source/RenderInterface.cpp
@@ -548,10 +548,12 @@ namespace osgLibRocket
 		{
 			node->getParent(0)->removeChild(node);
 		}
-      while(node->referenceCount() > 0)
-      {
-		   node->unref();
-      }
+		unsigned int refcount = node->referenceCount();
+		while(refcount > 0)
+		{
+			node->unref();
+			--refcount;
+		}
 	}
 
 	/// Called by Rocket when it wants to enable or disable scissoring to clip content.
