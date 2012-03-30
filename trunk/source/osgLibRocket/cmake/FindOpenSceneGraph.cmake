273ffa7570124045c8958b63e897192c2f8a1a0a
martinscheffler@gmail.com <martinscheffler@gmail.com@fe1f96f2-e110-b4a2-0c9b-42d904da81c5>
2012-03-30 07:33:46 +0000

    reworking library finding in build system. DTENTITY_EXT_DEP can now be changed from cmake-gui. Setting this will now change the standard cmake variables CMAKE_INCLUDE_PATH and CMAKE_LIBRARY_PATH.

--- trunk/source/osgLibRocket/cmake/FindOpenSceneGraph.cmake
+++ trunk/source/osgLibRocket/cmake/FindOpenSceneGraph.cmake
@@ -34,14 +34,11 @@ SET(OSG_ROOT_DIR $ENV{OSG_DIR} CACHE PATH "Root directory of OSG source tree")
 SET(OSG_BUILD_DIR ${OSG_ROOT_DIR} CACHE PATH "Root directory for OSG build to use")
 
 FIND_PATH(OSG_INCLUDE_DIRS osg/Program PATHS
-   $ENV{VIEWER_EXT_DEP}/include
    ${OSG_ROOT_DIR}/include
    $ENV{OSG_DIR}
    $ENV{OSG_DIR}/include
-	$ENV{OSG_INC}
-	
-	$ENV{DELTA3D_EXT}
-	$ENV{DELTA3D_EXT}/lib
+   $ENV{OSG_INC}
+   ${CMAKE_INCLUDE_PATH}	
 )
 
 # Also add the build-specific includes
@@ -63,7 +60,7 @@ SET(OSG_AVAILABLE_LIBS
     Viewer
 )
 
-SET(libpath $ENV{VIEWER_EXT_DEP}/lib $ENV{DELTA3D_EXT}/lib ${OSG_BUILD_DIR}/lib)
+SET(libpath ${CMAKE_LIBRARY_PATH} $ENV{DELTA3D_EXT}/lib ${OSG_BUILD_DIR}/lib)
 IF(NOT "${libpath}" STREQUAL "")
   FILE(TO_CMAKE_PATH "${libpath}" libpath)
 ENDIF(NOT "${libpath}" STREQUAL "")
