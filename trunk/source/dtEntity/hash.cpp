6f5dfe653796471a624c7930631db2b90737e7ee
martinscheffler@gmail.com <martinscheffler@gmail.com@fe1f96f2-e110-b4a2-0c9b-42d904da81c5>
2012-07-03 02:31:07 +0000

    updated murmurhash 3 to trunk (no real changes there)

--- trunk/source/dtEntity/hash.cpp
+++ trunk/source/dtEntity/hash.cpp
@@ -99,8 +99,8 @@ void MurmurHash3_x86_32 ( const void * key, int len,
 
   uint32_t h1 = seed;
 
-  uint32_t c1 = 0xcc9e2d51;
-  uint32_t c2 = 0x1b873593;
+  const uint32_t c1 = 0xcc9e2d51;
+  const uint32_t c2 = 0x1b873593;
 
   //----------
   // body
@@ -114,9 +114,9 @@ void MurmurHash3_x86_32 ( const void * key, int len,
     k1 *= c1;
     k1 = ROTL32(k1,15);
     k1 *= c2;
-
+    
     h1 ^= k1;
-    h1 = ROTL32(h1,13);
+    h1 = ROTL32(h1,13); 
     h1 = h1*5+0xe6546b64;
   }
 
@@ -143,7 +143,7 @@ void MurmurHash3_x86_32 ( const void * key, int len,
   h1 = fmix(h1);
 
   *(uint32_t*)out = h1;
-}
+} 
 
 //-----------------------------------------------------------------------------
 
@@ -158,10 +158,10 @@ void MurmurHash3_x86_128 ( const void * key, const int len,
   uint32_t h3 = seed;
   uint32_t h4 = seed;
 
-  uint32_t c1 = 0x239b961b;
-  uint32_t c2 = 0xab0e9789;
-  uint32_t c3 = 0x38b34ae5;
-  uint32_t c4 = 0xa1e38b93;
+  const uint32_t c1 = 0x239b961b; 
+  const uint32_t c2 = 0xab0e9789;
+  const uint32_t c3 = 0x38b34ae5; 
+  const uint32_t c4 = 0xa1e38b93;
 
   //----------
   // body
@@ -261,8 +261,8 @@ void MurmurHash3_x64_128 ( const void * key, const int len,
   uint64_t h1 = seed;
   uint64_t h2 = seed;
 
-  uint64_t c1 = BIG_CONSTANT(0x87c37b91114253d5);
-  uint64_t c2 = BIG_CONSTANT(0x4cf5ad432745937f);
+  const uint64_t c1 = BIG_CONSTANT(0x87c37b91114253d5);
+  const uint64_t c2 = BIG_CONSTANT(0x4cf5ad432745937f);
 
   //----------
   // body
